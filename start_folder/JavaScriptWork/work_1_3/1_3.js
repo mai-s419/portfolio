16f7d598ee84f1cc84be6f71574b1ed6c787e21b
mai shintani <mayixingu@gmail.com>
2026-03-14 21:57:28 +0900

    update files

---
+++ start_folder/JavaScriptWork/work_1_3/1_3.js
@@ -0,0 +1,6 @@
+const getTotalPrice = (price,quantity) => price * quantity;
+const addTax = total => total * 1.1;
+const total = getTotalPrice(1000, 2); // → 2000
+console.log(`税抜金額は${total}円です`);
+const taxedTotal = addTax(total);     // → 2200
+console.log(`税込金額は${taxedTotal}円です`);
