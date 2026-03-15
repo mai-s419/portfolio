4a584dd4f34ab4f10373cd614fb4dc4bcaca78a5
mai shintani <mayixingu@gmail.com>
2026-03-15 13:57:33 +0900

    update portfolio

---
+++ start_folder/javascript/githubsearch.js
@@ -0,0 +1,25 @@
+document.getElementById("searchbtn").addEventListener("click", () =>{
+    const username = document.getElementById("username").value.trim();
+    const resultDiv = document.getElementById("result");
+
+    if (!username) {
+        resultDiv.innerHTML = "<p>ユーザー名を入力してください</p>"
+        return;
+    }
+    fetch(`https://api.github.com/users/${username}`)
+    .then(res => {
+        if (!res.ok) throw new Error("ユーザーが見つかりませんでした");
+        return res.json();
+    })
+    .then(data => {
+        resultDiv.innerHTML = `
+        <p><strong>ユーザー名:</Strong> ${data.login}</p>
+        <p><strong>名前:</Strong> ${data.name || "未設定"}</p>
+        <p><strong>フォロワー数:</Strong> ${data.public_repos}</p>
+        <img src="${data.avatar_url}" width="100" alt"アイコン画像"/>
+        `;
+    })
+    .catch(err => {
+        resultDiv.innerHTML = `<p style="color:red;">${err.message}</p>`;
+    })
+})
